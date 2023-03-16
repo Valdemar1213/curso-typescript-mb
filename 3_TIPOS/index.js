62f709fb0f930d2a143cab3f00859fcba615ea3c
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-03-16 20:19:50 -0300

    Tipos

--- 3_TIPOS/index.js
+++ 3_TIPOS/index.js
@@ -12,3 +12,38 @@ console.log(nums[0]);
 // 3- Any
 const arr1 = [1, "teste", true, [], { nome: "Valdemar" }];
 console.log(arr1);
+// 4- Parametros tipados
+function soma(a, b) {
+    console.log(a + b);
+}
+soma(4, 5);
+// 5- Retorno de função
+function greeting(name) {
+    return `Olá ${name}`;
+}
+console.log(greeting("Valdemar"));
+// 6- Funções anônimas
+setTimeout(function () {
+    const salary = 1000;
+    // console.log(parseFloat(salary));
+    // console.log(salary);
+}, 2000);
+// 7- Tipos de Objetos
+function passCordinates(coord) {
+    console.log("X coordinates: " + coord.x);
+    console.log("Y coordinates: " + coord.y);
+}
+const objCoord = { x: 329, y: 84.2 };
+passCordinates(objCoord);
+const pessoaObj = {
+    nome: "Valdemar",
+    sobrenome: "Santana",
+};
+console.log(pessoaObj);
+// 8- Propriedades opcionais
+function showNumbers(a, b, c) {
+    console.log(`A: ${a}`);
+    console.log(`B: ${b}`);
+    console.log(`C: ${c}`);
+}
+showNumbers(1, 2);
--- 3_TIPOS/index.ts
+++ 3_TIPOS/index.ts
@@ -18,3 +18,50 @@ console.log(nums[0]);
 const arr1: any = [1, "teste", true, [], { nome: "Valdemar" }];
 
 console.log(arr1);
+
+// 4- Parametros tipados
+function soma(a: number, b: number) {
+  console.log(a + b);
+}
+
+soma(4, 5);
+
+// 5- Retorno de função
+function greeting(name: string): string {
+  return `Olá ${name}`;
+}
+
+console.log(greeting("Valdemar"));
+
+// 6- Funções anônimas
+setTimeout(function () {
+  const salary: number = 1000;
+
+  // console.log(parseFloat(salary));
+  // console.log(salary);
+}, 2000);
+
+// 7- Tipos de Objetos
+function passCordinates(coord: { x: number; y: number }) {
+  console.log("X coordinates: " + coord.x);
+  console.log("Y coordinates: " + coord.y);
+}
+
+const objCoord = { x: 329, y: 84.2 };
+passCordinates(objCoord);
+
+const pessoaObj: { nome: string; sobrenome: string } = {
+  nome: "Valdemar",
+  sobrenome: "Santana",
+};
+
+console.log(pessoaObj);
+
+// 8- Propriedades opcionais
+function showNumbers(a: number, b: number, c?: number) {
+  console.log(`A: ${a}`);
+  console.log(`B: ${b}`);
+  console.log(`C: ${c}`);
+}
+
+showNumbers(1, 2);
