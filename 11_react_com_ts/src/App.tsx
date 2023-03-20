0d9e1b7a023ca922e0349b002fe792fcdc1fdd5c
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-03-20 19:01:32 -0300

    TS com react

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,10 +1,88 @@
-import React from "react";
+import React, { createContext } from "react";
+
+// 4- Importação de componentes
+import FirstComponent from "./components/FirstComponent";
+
+// 5- Desestruturando props
+import SecondComponent from "./components/SecondComponent";
+import Destructuring, { Category } from "./components/Destructuring";
+
+// 6- useState
+import State from "./components/State";
+
+// 10- utilizando context
+import Context from "./components/Context";
+
+// 8- Type
+type textOrNull = string | null;
+
+// type fixed = "Isso" | "Ou" | "Aquilo";
+
+// 9- context
+interface IAppContext {
+  language: string;
+  framework: string;
+  projects: number;
+}
+
+export const AppContext = createContext<IAppContext | null>(null);
 
 function App() {
+  // 1- Variáveis
+  const name: string = "Valdemar";
+  const age: number = 30;
+  const isWorking: boolean = true;
+
+  // 2- Funções
+  const userGreeting = (name: string) => {
+    return `Olá, ${name}!`;
+  };
+
+  // 8- Type
+  const myText: textOrNull = "Tem algum texto aqui!";
+  let mySecondText: textOrNull = null;
+
+  // mySecondText = "opa";
+
+  // const testandoFixed: fixed = "Isso";
+
+  // 9- context
+  const contextValue: IAppContext = {
+    language: "JavaScript",
+    framework: "Express",
+    projects: 5,
+  };
+
   return (
-    <div className="App">
-      <h1>Typescript com React</h1>
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>Typescript com React</h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isWorking && <p>Está trabalhando!</p>}
+        <h3>{userGreeting(name)}</h3>
+        <FirstComponent />
+        <SecondComponent name="Segundo" />
+        <Destructuring
+          title="Primeiro post"
+          content="Algum conteúdo"
+          commentsQty={10}
+          tags={["ts", "js"]}
+          category={Category.TS}
+        />
+        <Destructuring
+          title="Segundo post"
+          content="Outro conteúdo"
+          commentsQty={13}
+          tags={["python"]}
+          category={Category.P}
+        />
+        <State />
+        {myText && <p>Tem texto na variável</p>}
+        {mySecondText && <p>Tem texto na variável</p>}
+        <Context />
+      </div>
+    </AppContext.Provider>
   );
 }
 
